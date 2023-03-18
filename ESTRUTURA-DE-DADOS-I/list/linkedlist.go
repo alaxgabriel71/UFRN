736397e823936da295d052aeff7faad48556b87b
Alax Gabriel <alaxgabriel71@gmail.com>
2023-03-18 13:52:04 -0300

    implementing the is empty method

--- ESTRUTURA-DE-DADOS-I/list/linkedlist.go
+++ ESTRUTURA-DE-DADOS-I/list/linkedlist.go
@@ -15,6 +15,15 @@ func (linkedlist *LinkedList) Init() {
 	linkedlist.size = 0
 }
 
+// returns if the linked list is empty or not
+func (linkedlist *LinkedList) isEmpty() bool {
+	if linkedlist.size == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
 // returns the linked list's size
 func (linkedlist *LinkedList) Size() int {
 	return linkedlist.size
