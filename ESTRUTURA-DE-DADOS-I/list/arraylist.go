0ed9391664689c39232537be9591d4fa6fbb252e
Alax Gabriel <alaxgabriel71@gmail.com>
2023-03-18 11:42:36 -0300

    adding the check capacity method

--- ESTRUTURA-DE-DADOS-I/list/arraylist.go
+++ ESTRUTURA-DE-DADOS-I/list/arraylist.go
@@ -5,11 +5,25 @@ type ArrayList struct {
 	size   int
 }
 
+// initialize the array's value with 10 of capacity
+// and the array's size with 0
 func (arraylist *ArrayList) Init() {
 	arraylist.values = make([]int, 10)
 	arraylist.size = 0
 }
 
+// returns the array's size
 func (arraylist *ArrayList) Size() int {
 	return arraylist.size
+}
+
+// duplicates the array's capacity if it's full
+func (arraylist *ArrayList) checkCapacity() {
+	if len(arraylist.values) == arraylist.size {
+		newarray := make([]int, len(arraylist.values)*2)
+		for i := 0; i < len(arraylist.values); i++ {
+			newarray[i] = arraylist.values[i]
+		}
+		arraylist.values = newarray
+	}
 }
